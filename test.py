c4009e40dc6a2d3041b300332d84879993296cce
BuildTools <unconfigured@null.spigotmc.org>
2020-04-28 16:50:55 +0900

    検索できるようにしてみた

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import subprocess, json, os, pathlib
+# coding= utf-8
+import subprocess, json, os, pathlib, glob, codecs, chardet
 
 print("current_dir: " + os.getcwd())
 
@@ -15,6 +16,8 @@ print(repositories_result.stdout)
 json_load = json.loads(repositories_result.stdout)
 repositories = json_load.get("repositories")
 
+search_word = "cpu"
+
 for repository in repositories:
     repository_name = repository.get("repositoryName")
     print("repository_name: " + repository_name)
@@ -25,3 +28,22 @@ for repository in repositories:
     else:
         result = subprocess.run(['git', 'clone', clone_url + repository_name], stdout = subprocess.PIPE, encoding='UTF-8', shell=True)
 
+    object_list = glob.glob("**", recursive=True)
+
+    for object in object_list:
+        if os.path.isfile(object):
+            with codecs.open(object, 'rb') as f:
+                lines = f.readlines()
+                for line in lines:
+                    char_dat = chardet.detect(line)
+                    encoding = char_dat.get("encoding")
+                    if encoding == None:
+                        line = repr(line)
+                    elif encoding == "Windows-1254" or encoding == "TIS-620" or encoding == "Windows-1252" or encoding == "windows-1253" or encoding == "Windows-1253" or encoding == "windows-1255":
+                        continue
+                    else:
+                        line = line.decode(encoding)
+                    if search_word in line:
+                        print(line)
+                        
+                    
