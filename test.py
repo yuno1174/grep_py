2da000d845fb6d355331710e05860e2aa6242bd0
BuildTools <unconfigured@null.spigotmc.org>
2020-04-28 14:45:44 +0900

    git cloneするようにした

--- test.py
+++ test.py
@@ -1,11 +1,27 @@
-import subprocess
-import json
+import subprocess, json, os, pathlib
 
-result = subprocess.run(['aws', 'codecommit', 'list-repositories'], stdout = subprocess.PIPE, encoding='UTF-8', shell=True)
-print(result.returncode)
-print(result.stdout)
+print("current_dir: " + os.getcwd())
 
-json_load = json.loads(result.stdout)
+clone_dir = "F:\Desktop\py\clone_dir"
+clone_url = "https://git-codecommit.ap-northeast-1.amazonaws.com/v1/repos/"
+
+print("mkdir実行")
+pathlib.Path(clone_dir).mkdir(parents=True, exist_ok=True)
+os.chdir(clone_dir)
+
+repositories_result = subprocess.run(['aws', 'codecommit', 'list-repositories'], stdout = subprocess.PIPE, encoding='UTF-8', shell=True)
+print(repositories_result.returncode)
+print(repositories_result.stdout)
+json_load = json.loads(repositories_result.stdout)
 repositories = json_load.get("repositories")
+
 for repository in repositories:
-    print("repository_name: " + repository.get("repositoryName"))
+    repository_name = repository.get("repositoryName")
+    print("repository_name: " + repository_name)
+    if os.path.exists(repository_name):
+        os.chdir(clone_dir + "/" + repository_name)
+        result = subprocess.run(['git', 'pull'], stdout = subprocess.PIPE, encoding='UTF-8', shell=True)
+        os.chdir(clone_dir)
+    else:
+        result = subprocess.run(['git', 'clone', clone_url + repository_name], stdout = subprocess.PIPE, encoding='UTF-8', shell=True)
+
